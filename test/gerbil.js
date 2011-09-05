8f1aea24350ec8123e652d32bdcf2059674717ee
Daniel Cadenas <dcadenas@gmail.com>
2011-09-05 18:24:30 -0300

    Fix assert_raise when nothing is raised and assertion counter

--- test/gerbil.js
+++ test/gerbil.js
@@ -5,9 +5,24 @@ scenario("Gerbil - Assertions", {
     assert(true);
     assert(1 == 1);
   },
+
   "should be able to assert a false statement": function(){
     assert_raise(Error, function(){
       assert(false)
-    });
+    })
+  },
+
+  "should fail when an assert_raise block doesn't raise an exception": function(){
+    var exception_was_raised = false
+
+    try {
+      assert_raise(Error, function(){
+        //we are not raising anything
+      });
+    } catch (exception) {
+      exception_was_raised = true;
+    }
+
+    assert(exception_was_raised);
   }
 });
