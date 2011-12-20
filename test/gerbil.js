4f3771b329bb59af67c363ddd55966e78fbcc026
Bruno Aguirre <yo@brunoaguirre.com>
2011-12-20 14:36:49 -0200

    Work within queues

--- test/gerbil.js
+++ test/gerbil.js
@@ -1,10 +1,11 @@
 if(typeof module != 'undefined')
-  scenario = require('../lib/gerbil.js');
+  scenario = require('../lib/gerbil.js').scenario;
 
 scenario("Gerbil - Assertions", {
   "should be able to assert": function(g){
     g.assert(true);
     g.assert(1 == 1);
+    g.assert(true != false);
   },
 
   "should be able to assert a false statement": function(g){
@@ -21,35 +22,6 @@ scenario("Gerbil - Assertions", {
     });
   },
 });
-
-var scenario_logger = {
-  counter: 0,
-  count: function(){ this.counter += 1; },
-}
-
-scenario("Gerbil - after, before, setup, cleanup", {
-  "setup": function(){
-    scenario_logger.count();
-  },
-
-  "before": function(){
-    scenario_logger.count();
-  },
-
-  "after": function(){
-    scenario_logger.count();
-  },
-
-  "cleanup": function(g){
-    scenario_logger.count();
-    g.assert_equal(scenario_logger.counter, 6);
-  },
-
-  "should run tests": function(g){ g.assert(true); },
-
-  "should run tests again": function(g){ g.assert(true); }
-});
-
 
 scenario("Gerbil - setTimeout", {
   "should work": function(g){
